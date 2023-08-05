7f1aa6acb3f31a83156507a0e113c9eda9d00e2b
İhsan Eren Delibaş <hsan Eren Deliba@DESKTOP-RPEDS99>
2023-08-05 11:46:46 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            groupBox2 = new GroupBox();
+            btnPlus = new Button();
+            btnMinus = new Button();
+            btnMultiply = new Button();
+            btnDivision = new Button();
+            txtProces = new TextBox();
+            textBox2 = new TextBox();
+            groupBox3 = new GroupBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button0);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(12, 105);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(130, 200);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(35, 35);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(88, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(35, 35);
+            button2.TabIndex = 1;
+            button2.Text = "3";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(47, 22);
+            button3.Name = "button3";
+            button3.Size = new Size(35, 35);
+            button3.TabIndex = 2;
+            button3.Text = "2";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(6, 63);
+            button4.Name = "button4";
+            button4.Size = new Size(35, 35);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(47, 63);
+            button5.Name = "button5";
+            button5.Size = new Size(35, 35);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(88, 63);
+            button6.Name = "button6";
+            button6.Size = new Size(35, 35);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(6, 104);
+            button7.Name = "button7";
+            button7.Size = new Size(35, 35);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(47, 104);
+            button8.Name = "button8";
+            button8.Size = new Size(35, 35);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(88, 104);
+            button9.Name = "button9";
+            button9.Size = new Size(35, 35);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            button0.Location = new Point(47, 145);
+            button0.Name = "button0";
+            button0.Size = new Size(35, 35);
+            button0.TabIndex = 9;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnDivision);
+            groupBox2.Controls.Add(btnMultiply);
+            groupBox2.Controls.Add(btnMinus);
+            groupBox2.Controls.Add(btnPlus);
+            groupBox2.Location = new Point(149, 105);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(49, 200);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Location = new Point(6, 22);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(35, 35);
+            btnPlus.TabIndex = 2;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Location = new Point(6, 63);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(35, 35);
+            btnMinus.TabIndex = 2;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            btnMultiply.Location = new Point(6, 104);
+            btnMultiply.Name = "btnMultiply";
+            btnMultiply.Size = new Size(35, 35);
+            btnMultiply.TabIndex = 2;
+            btnMultiply.Text = "*";
+            btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnDivision
+            // 
+            btnDivision.Location = new Point(6, 145);
+            btnDivision.Name = "btnDivision";
+            btnDivision.Size = new Size(35, 35);
+            btnDivision.TabIndex = 2;
+            btnDivision.Text = "/";
+            btnDivision.UseVisualStyleBackColor = true;
+            // 
+            // txtProces
+            // 
+            txtProces.Location = new Point(6, 22);
+            txtProces.Name = "txtProces";
+            txtProces.Size = new Size(175, 23);
+            txtProces.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(55, 51);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(126, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label1);
+            groupBox3.Controls.Add(txtProces);
+            groupBox3.Controls.Add(textBox2);
+            groupBox3.Location = new Point(12, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(187, 87);
+            groupBox3.TabIndex = 4;
+            groupBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Sonuç:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(210, 315);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Hesap Makinesi";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button0;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Button btnDivision;
+        private Button btnMultiply;
+        private Button btnMinus;
+        private Button btnPlus;
+        private TextBox txtProces;
+        private TextBox textBox2;
+        private GroupBox groupBox3;
+        private Label label1;
+    }
+}
